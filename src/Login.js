cb8db1579f890598d5d563df4a77089cf3594761
Sudhirsin <lion.sud.k@gmail.com>
2020-08-22 21:54:55 +0530

    added the first page spotify and login button

---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="login">
+            {/* Spotify Logo */}
+            <img 
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+                alt="spotify-logo"
+            />
+
+            {/* Login with spotify button */}
+            <a>LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/keys.js
@@ -0,0 +1,2 @@
+// spotify client id
+clientID =  b1165d82b9834b3cb71a038be5f3eac0;
